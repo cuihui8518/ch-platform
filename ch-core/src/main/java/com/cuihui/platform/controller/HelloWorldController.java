6e02f5f4f2f1898559f417166f9c0d206e131a53
cuihui8518 <15018543235@163.com>
2018-08-08 17:09:12 +0800

    新建项目

---
+++ ch-core/src/main/java/com/cuihui/platform/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.cuihui.platform.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldController.class);
+
+    @RequestMapping("/hello")
+    public String hello() {
+        LOGGER.info("call method hello");
+        return "Hello World";
+    }
+}
