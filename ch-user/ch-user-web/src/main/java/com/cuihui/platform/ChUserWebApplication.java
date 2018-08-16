8d7bad8b0f97569df62ca9ebafdd8ee64f371067
cuihui8518 <15018543235@163.com>
2018-08-16 15:38:06 +0800

    新建项目

---
+++ ch-user/ch-user-web/src/main/java/com/cuihui/platform/ChUserWebApplication.java
@@ -0,0 +1,12 @@
+package com.cuihui.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChUserWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChUserWebApplication.class, args);
+    }
+}
